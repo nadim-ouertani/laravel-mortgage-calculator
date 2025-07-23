2fb63e35ae195e8785068aa2b69f0d4ce65a2d2c
Nadim Ouertani <nadimouertani@hotmail.com>
2025-07-23 21:45:00 +0400

    Add loan calculation endpoint

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,5 @@ Route::get('/user', function (Request $request) {
 
 // Mortgage loan calculator API routes
 Route::prefix('loans')->group(function () {
-    Route::post('/calculate', [LoanController::class, 'calculateLoan']);
-    Route::get('/{id}', [LoanController::class, 'show']);
-    Route::delete('/{id}', [LoanController::class, 'destroy']);
+    Route::post('/calculate', [LoanController::class, 'calculate']);
 });
