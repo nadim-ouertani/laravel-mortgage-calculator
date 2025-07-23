eb5d6b86af33d98fccac32798bb5b962744ff676
Nadim Ouertani <nadimouertani@hotmail.com>
2025-07-23 21:15:00 +0400

    Create API controller structure

--- app/Http/Requests/LoanCalculationRequest.php
+++ app/Http/Requests/LoanCalculationRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Domain\Loan\Constants\LoanValidationConstants;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoanCalculationRequest extends FormRequest
@@ -15,45 +14,20 @@ class LoanCalculationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'loan_amount' => [
-                'required',
-                'numeric',
-                'min:' . LoanValidationConstants::MIN_LOAN_AMOUNT,
-                'max:' . LoanValidationConstants::MAX_LOAN_AMOUNT
-            ],
-            'annual_interest_rate' => [
-                'required',
-                'numeric',
-                'min:' . LoanValidationConstants::MIN_INTEREST_RATE
-            ],
-            'loan_term_years' => [
-                'required',
-                'integer',
-                'min:' . LoanValidationConstants::MIN_LOAN_TERM_YEARS
-            ],
-            'monthly_extra_payment' => [
-                'sometimes',
-                'numeric',
-                'min:' . LoanValidationConstants::MIN_EXTRA_PAYMENT
-            ]
+            'loan_amount' => 'required|numeric|min:5000',
+            'annual_interest_rate' => 'required|numeric|min:0',
+            'loan_term_years' => 'required|integer|min:1',
+            'monthly_extra_payment' => 'nullable|numeric|min:0',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'loan_amount.required' => 'Loan amount is required',
-            'loan_amount.numeric' => 'Loan amount must be a valid number',
-            'loan_amount.min' => 'Minimum loan amount is AED 5,000',
-            'loan_amount.max' => 'Maximum loan amount is AED 4,000,000',
-            'annual_interest_rate.required' => 'Annual interest rate is required',
-            'annual_interest_rate.numeric' => 'Interest rate must be a valid number',
+            'loan_amount.min' => 'Loan amount must be at least AED 5,000',
             'annual_interest_rate.min' => 'Interest rate cannot be negative',
-            'loan_term_years.required' => 'Loan term is required',
-            'loan_term_years.integer' => 'Loan term must be a whole number of years',
             'loan_term_years.min' => 'Loan term must be at least 1 year',
-            'monthly_extra_payment.numeric' => 'Extra payment must be a valid number',
-            'monthly_extra_payment.min' => 'Extra payment cannot be negative'
+            'monthly_extra_payment.min' => 'Extra payment cannot be negative',
         ];
     }
 }
